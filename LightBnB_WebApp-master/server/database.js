1ab5080f2b2f1c94aaf13f53fbda2c8de38829a9
Abanoub Shenouda <Abanoub Shenouda>
2021-06-24 21:07:30 -0400

    Add add property listing function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -142,7 +142,9 @@ const getAllProperties = (options, limit = 10) => {
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(Number(options.minimum_price_per_night) * 100);
     queryParams.push(Number(options.maximum_price_per_night) * 100);
-    queryString += `${command} properties.cost_per_night > $${queryParams.length - 1}
+    queryString += `${command} properties.cost_per_night > $${
+      queryParams.length - 1
+    }
      AND properties.cost_per_night < $${queryParams.length}`;
     command = "AND";
   }
@@ -153,7 +155,6 @@ const getAllProperties = (options, limit = 10) => {
     queryString += `${command} property_reviews.rating > $${queryParams.length}`;
   }
 
-    
   // 4
   queryParams.push(limit);
   queryString += `
@@ -175,10 +176,66 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+// const addProperty = function (property) {
+//   const propertyId = Object.keys(properties).length + 1;
+//   property.id = propertyId;
+//   properties[propertyId] = property;
+//   return Promise.resolve(property);
+// };
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log(property);
+
+  return pool
+    .query(
+      `INSERT INTO properties(
+        owner_id,
+        title,
+        description,
+        thumbnail_photo_url,
+        cover_photo_url,
+        cost_per_night,
+        street,
+        city,
+        province,
+        post_code,
+        country,
+        parking_spaces,
+        number_of_bathrooms,
+        number_of_bedrooms
+        ) VALUES(
+          $1,
+          $2,
+          $3,
+          $4,
+          $5,
+          $6,
+          $7,
+          $8,
+          $9,
+          $10,
+          $11,
+          $12,
+          $13,
+          $14
+          ) RETURNING *;`,
+      [
+        property.owner_id,
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        property.cost_per_night,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+        property.country,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms
+      ]
+    )
+    .then()
+    .catch((err) => console.log(err.message));
 };
 exports.addProperty = addProperty;
